9fe4340c3d233da4401f5a0ebd8226ba3970d490
fleish <cfleisher3@gatech.edu>
2019-08-26 13:45:35 -0400

    adds flake8 to env and lints InsaneLearner

--- assess_learners/InsaneLearner.py
+++ assess_learners/InsaneLearner.py
@@ -5,14 +5,15 @@ from BagLearner import BagLearner
 
 class InsaneLearner(object):
     def __init__(self, verbose=False):
-        self.learners = [BagLearner(learner=LinRegLearner, verbose=verbose, bags=20) for _ in range(20)]
+        opts = {'learner': LinRegLearner, 'verbose': verbose, 'bags': 20}
+        self.lrns = [BagLearner(**opts) for _ in range(20)]
 
     def author(self):
         return 'cfleisher3'
 
     def addEvidence(self, dataX, dataY):
-        for learner in self.learners:
+        for learner in self.lrns:
             learner.addEvidence(dataX, dataY)
 
-    def query(self, points):
-        return np.mean(np.array([learner.query(points) for learner in self.learners]), axis=0)
+    def query(self, X):
+        return np.mean(np.array([lrn.query(X) for lrn in self.lrns]), axis=0)
