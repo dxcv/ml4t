f23a22a23debe356f892b6f4c239aa8454a35455
fleish <cfleish@gmail.com>
2019-08-25 22:33:05 -0400

    shell RTLearner

---
+++ assess_learners/RTLearner.py
@@ -0,0 +1,12 @@
+class RTLearner(object):
+    def __init__(self):
+        pass
+
+    def author(self):
+        return 'cfleisher3'
+
+    def addEvidence(self, dataX, dataY):
+        pass
+
+    def query(self, points):
+        pass
